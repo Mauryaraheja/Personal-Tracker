91b3a42490daa3cd512f90fc947608c6020b36c9
Mauryaraheja <mraheja_be24@thapar.edu>
2026-09-05 11:06:52 +0530

    Main file restructred , now it becomes a starting thin point , and the main logic(the core lies in src in different files)

--- src/personaltracker/__init__.py
+++ src/personaltracker/__init__.py
@@ -1,2 +1,4 @@
-def main() -> None:
-    print("Hello from personaltracker!")
+from .roadmap import get_skill_roadmap
+from .cv_parser import extract_text_from_pdf
+
+__all__ = ["get_skill_roadmap", "extract_text_from_pdf"]
