45ca176bfb641df830e7c433395747c3dc3fc75b
Mauryaraheja <mraheja_be24@thapar.edu>
2026-09-07 13:50:48 +0530

    Added tracker path into the init

--- src/personaltracker/__init__.py
+++ src/personaltracker/__init__.py
@@ -1,5 +1,6 @@
 from .roadmap import get_skill_roadmap
 from .cv_parser import extract_text_from_pdf
 from .gap_analysis import get_skill_gaps
+from .tracker import get_or_create_roadmap, get_tracker_items, update_tracker_status
 
 __all__ = ["get_skill_roadmap", "extract_text_from_pdf", "get_skill_gaps"]
