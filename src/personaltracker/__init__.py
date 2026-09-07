47c91b7a871a1b321f99f02306359f26590b52ac
Mauryaraheja <mraheja_be24@thapar.edu>
2026-09-07 17:38:10 +0530

    Added market analysis , this feature will fetch real job descriptions from indeed , greenhouse , type job portals

--- src/personaltracker/__init__.py
+++ src/personaltracker/__init__.py
@@ -2,5 +2,7 @@ from .roadmap import get_skill_roadmap
 from .cv_parser import extract_text_from_pdf
 from .gap_analysis import get_skill_gaps
 from .tracker import get_or_create_roadmap, get_tracker_items, update_tracker_status
+from .market_validation import get_market_validation
+
 
 __all__ = ["get_skill_roadmap", "extract_text_from_pdf", "get_skill_gaps"]
